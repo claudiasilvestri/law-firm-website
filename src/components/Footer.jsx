0a11c02391303cc05b1a7975b0c5b9a5da114580
Claudia Silvestri <claudiasilvestri.dev@gmail.com>
2026-06-04 11:54:29 +0200

    Add iubenda policies

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,6 +13,22 @@ export default function Footer() {
         P. IVA 07282120729
       </p>
 
+      <div className="footer-policies">
+        <a
+          href="https://www.iubenda.com/privacy-policy/30019020"
+          className="iubenda-white iubenda-noiframe iubenda-embed"
+        >
+          Privacy Policy
+        </a>
+
+        <a
+          href="https://www.iubenda.com/privacy-policy/30019020/cookie-policy"
+          className="iubenda-white iubenda-noiframe iubenda-embed"
+        >
+          Cookie Policy
+        </a>
+      </div>
+
       <p className="credits">
         Sito web realizzato da{" "}
         <a
